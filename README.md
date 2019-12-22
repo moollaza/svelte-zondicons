d7008ba85a5a44112fca8c6346ffa9886edc7c24
Zaahir Moolla <moollaza@fastmail.fm>
2019-12-21 19:52:32 -0500

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+## Svelte Zondicons
+
+[![npm version](https://img.shields.io/npm/v/svelte-zondicons.svg?style=flat-square)](https://www.npmjs.com/package/svelte-zondicons)
+
+
+### Installation
+
+    yarn add -D svelte-zondicons
+
+or NPM
+
+    npm install --save-dev svelte-zondicons
+
+### Usage
+
+```html
+<script>
+  import { AirplaneIcon, ArrowLeftIcon } from 'svelte-zondicons';
+</script>
+
+<AirplaneIcon />
+<ArrowLeftIcon />
+
+<!-- Or using as custom elements -->
+<airplane-icon />
+<arrow-left-icon />
+```
+
+## Credit
+[Zondicons](https://www.zondicons.com/) is a free SVG icon set created by [Steve Schoger](https://twitter.com/steveschoger).
---
+++ bin/build.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const fsp = require("fs").promises;
+const path = require("path");
+const util = require("util");
+const globby = require('globby');
+const pascalCase = require("pascalcase");
+
+const getComponentName = name => {
+    return pascalCase(name)
+};
+
+const rootDir = path.join(__dirname, "..");
+const svgDir = path.join(rootDir, "./svg");
+
+const iconsDir = path.join(rootDir, "./src/icons");
+if (!fs.existsSync(iconsDir)) {
+    fs.mkdirSync(iconsDir, { recursive: true });
+}
+
+// RUN
+(async () => {
+    let icons = {};
+    let indexContent = "";
+
+    try {
+        const files = await globby(`${svgDir}/*.svg`);
+        await Promise.all(files.map(async function (file) {
+            let content = await fsp.readFile(file, 'utf-8')
+            const svgInner = content.match(/<p(ath|olygon) [^>]+>/)[0];
+            let name = path.basename(file, '.svg');
+            icons[name] = svgInner;
+        }));
+
+        await Promise.all(Object.keys(icons).map(async function (name) {
+            const str = icons[name];
+            const componentName = getComponentName(name);
+            indexContent += `export { default as ${componentName}Icon } from './icons/${componentName}.svelte';\n`;
+            const componentContent = `<svelte:options tag="${name}-icon"/>\n
+<script>
+  export let color = 'currentColor';
+  export let size = 24;
+  let className = '';
+  export { className as class };
+</script>
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="{size}"
+  height="{size}"
+  viewBox="0 0 20 20"
+  fill="none"
+  class={className}
+  stroke={color}
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  >
+  ${str}
+</svg>`;
+            await fsp.writeFile(path.join(iconsDir, componentName + ".svelte"), componentContent);
+        }));
+    } catch (error) {
+        console.error(error);
+    }
+
+    try {
+        await fsp.writeFile(path.join(rootDir, "src/index.js"), indexContent);
+    } catch (error) {
+        console.error(error);
+    }
+})();
